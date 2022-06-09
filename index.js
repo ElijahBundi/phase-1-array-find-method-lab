6a5204aac3ad2ce661f72ac1b65bc832d93a4fd9
Elijah <bundielly@gmail.com>
2022-06-09 20:58:02 +0300

    Iteration

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // code your solution here
+function superbowlWin(array) {
+    for (let i of array) {
+        if (i.result === 'W') {
+            return i.year
+        }
+    }
+}
+
+record.find(superbowlWin)
